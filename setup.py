24ac80050304f21cdd422fbc407ea2e0763d5aa8
Longbowou <blandedaniel@gmail.com>
2021-07-28 20:31:06 +0000

    [update] to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django-ktdatatable-view",
-    version="1.0.0",
+    version="1.1.0",
     author="Longbowou",
     author_email="blandedaniel@gmail.com",
     description="Django KTDatatable View",
